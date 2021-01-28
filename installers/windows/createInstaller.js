c9bea05c1006f9fd57aee5bc8a9131994b91866c
Jonathan Schneider <jonathan.schneider@gmx.de>
2021-01-28 21:28:00 +0100

    Add Windows installer

---
+++ installers/windows/createInstaller.js
@@ -0,0 +1,25 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller;
+const path = require('path');
+
+getInstallerConfig()
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.error(error.message || error);
+    process.exit(1);
+  });
+
+function getInstallerConfig () {
+  console.log('creating windows installer');
+  const rootPath = path.join('./');
+  const outPath = path.join(rootPath, 'release-builds');
+
+  return Promise.resolve({
+    appDirectory: path.join(outPath, 'ProPresenter-Suite-win32-ia32/'),
+    authors: 'Jonathan Schneider',
+    noMsi: true,
+    outputDirectory: path.join(outPath, 'windows-installer'),
+    exe: 'ProPresenter-Suite.exe',
+    setupExe: 'ProPresenter-Suite-Installer.exe',
+    setupIcon: path.join(rootPath, 'assets', 'icons', 'icon.ico')
+  });
+}
